a306720ca42ef5de5f7212c2ca425aa44df2189d
Ali Saleh <alifadisaleh99@gmail.com>
2024-06-16 17:37:17 +0300

    Role & Permissions entities

---
+++ src/entities/permissions.entity.ts
@@ -0,0 +1,16 @@
+import {
+    Entity,
+    Column,
+    OneToMany,
+} from 'typeorm'
+import model from './mode.entity';
+import { RolePermissions } from './role_has_permissions.entity';
+
+@Entity()
+export class Permission extends model {
+    @Column()
+    name: string
+
+    @OneToMany(() => RolePermissions, rolePermissions => rolePermissions.permission)
+    rolePermissions: RolePermissions[]
+} 
